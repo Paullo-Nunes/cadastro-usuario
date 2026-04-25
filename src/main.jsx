ef3fb4d517e4dd110a98a32ae3269214ace14818
Dev Js <alienwaredev369@outlook.com>
2026-04-25 14:19:13 -0300

    Foi importado o router provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { GlobalStyles } from './GlobalStyles.js'
-import App from './pages/Home'
+// import App from './pages/Home'
+import { RouterProvider } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider />
     <GlobalStyles />
   </StrictMode>,
 )
