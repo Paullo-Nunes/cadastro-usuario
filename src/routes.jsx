46ef0bd5c8adcb385deee7047c0014421d350753
Dev Js <alienwaredev369@outlook.com>
2026-04-25 12:51:30 -0300

    Foi criada duas rotas

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,3 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import Home from './pages/Home'
+import ListUsers from "./pages/ListUsers";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/lista-de-usuarios',
+        element: <ListUsers />
+    }
+])
