c2b3a5cd1f4d0da8ad4c41b6d4890535e765a1af
Dev Js <alienwaredev369@outlook.com>
2026-04-26 14:10:57 -0300

    Função ListUsers criadas

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,9 +1,11 @@
+import { Button } from "../../components/Button/styles"
 
 function ListUsers() {
 
     return(
         <div>
             <h1>Listagem de Usuários</h1>
+            <Button>Voltar</Button>
         </div>
     )
 }
