c75d5d6d7f6d631aaea76e89c9467e4aded1f960
Dev Js <alienwaredev369@outlook.com>
2026-05-01 17:41:35 -0300

    Foi criado um componente Background que é a imagem

---
+++ src/components/TopBackground/index.jsx
@@ -0,0 +1,15 @@
+import UsersImage from '../../assets/users.png'
+
+import { Background } from './styles'
+
+function TopBackground() {
+    return (
+        <Background>
+
+            <img src={UsersImage} alt="imagem dos usuários" />
+
+        </Background>
+    )
+}
+
+export default TopBackground
