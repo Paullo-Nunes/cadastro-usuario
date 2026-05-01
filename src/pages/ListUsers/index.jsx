b17ff1f0041de24d4430865b640452702c32e32e
Dev Js <alienwaredev369@outlook.com>
2026-05-01 17:49:52 -0300

    Foi incrementado o componente TopBackground a ListUsers

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,9 +1,11 @@
 import { Button } from "../../components/Button/styles"
+import TopBackground from "../../components/TopBackground"
 
 function ListUsers() {
 
     return (
         <div>
+            <TopBackground />
             <h1>Listagem de Usuários</h1>
             <Button>Voltar</Button>
         </div>
