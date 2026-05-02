6df0452eccf9b4f6a11f5b48370f9d41dc496781
Dev Js <alienwaredev369@outlook.com>
2026-05-02 14:23:45 -0300

    Foi adicionada a condição de cor no background e na borda do Button.

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -3,9 +3,9 @@ import styled from "styled-components";
 export const Button = styled.button`
 padding: 12px 20px;
 border-radius: 30px;
-border: none;
+border: ${props => props.theme === 'primary' ? 'none' : '1px solid #fff'};
 font-size: 16px;
-background: linear-gradient(180deg, #fe7e5d 0%, #ff6378 100%);
+background: ${props => props.theme === 'primary' ? 'linear-gradient(180deg, #fe7e5d 0%, #ff6378 100%)' : 'transparent'};
 color: #fff;
 width: fit-content;
 cursor: pointer;
