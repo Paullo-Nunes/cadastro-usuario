f7c243d1eaf46d647637d2e361a7aa56e01161a9
Dev Js <alienwaredev369@outlook.com>
2026-05-07 06:29:23 -0300

    Fazendo a requisição da api do banco com useEffect

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,7 +1,22 @@
 import { Button } from "../../components/Button/styles"
 import TopBackground from "../../components/TopBackground"
+import { useEffect } from "react"
+import api from "../../services/api"
 
 function ListUsers() {
+
+    useEffect(() => {
+        async function getUsers() {
+            const usersFromApi = await api.get('/usuario')
+            console.log(usersFromApi)
+        }
+
+        getUsers()
+
+    }, [])
+
+    //Toda vez que a tela carrega, o useEffect é chamado
+    //Toda vez que uma determinada variável muda de valor, ele é chamado
 
     return (
         <div>
